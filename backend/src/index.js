d80da4431d850946597712c15de9a48c40ecbb2a
Sameep <Sameepagarwal232@gmail.com>
2024-03-12 21:27:59 +0530

    eight commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,22 +1,26 @@
 const express = require('express');
 const app = express();
-const cookieParser = require('cookie-parser');
+
 const PORT = process.env.PORT || 8000;
 const BACKEND_LINK = `localhost:${PORT}`;
-const cors = require('cors');
+
 const dotenv = require('dotenv');
 dotenv.config({ Path: "../.env" });
+
+const cookieParser = require('cookie-parser');
 app.use(cookieParser());
 
+const cors = require('cors');
 app.use(cors({
-    origin: 'https://demo-erp-frontend.onrender.com',
+    origin: ['https://demo-erp-frontend.onrender.com', 'http://localhost:5173'],
     credentials: true
 }));
-app.use(express.json());
-app.use(require('./router/router'));
 
+app.use(express.json());
+
+app.use(require('./Router/router'));
 require("./db/conn");
 
-app.listen(8000, () => {
+app.listen(PORT, () => {
     console.log(`Listening to port number ${PORT}, Running on ${BACKEND_LINK}`);
 });
